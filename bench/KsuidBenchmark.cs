f2260d6627dc37579850fe5f4484225a07b46a9c
Steve Warren <steve-warren@users.noreply.github.com>
2022-05-23 20:51:49 -0500

    convert tabs to spaces.

--- bench/KsuidBenchmark.cs
+++ bench/KsuidBenchmark.cs
@@ -6,12 +6,12 @@ namespace Ksuid.Bench;
 [MemoryDiagnoser]
 public class KsuidBenchmark
 {
-	readonly RandomNumberGenerator _rng = RandomNumberGenerator.Create();
+    readonly RandomNumberGenerator _rng = RandomNumberGenerator.Create();
 
-	[Benchmark]
-	public void KsuidTest()
-	{
-		ReadOnlySpan<char> prefix = "cu_".AsSpan();
-		_ = KsuidFactory.New(_rng, DateTime.UtcNow, prefix);
-	}
+    [Benchmark]
+    public void KsuidTest()
+    {
+        ReadOnlySpan<char> prefix = "cu_".AsSpan();
+        _ = KsuidFactory.New(_rng, DateTime.UtcNow, prefix);
+    }
 }
