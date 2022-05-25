c4c9823ac1afe0e6f829fe7e8032ebb375955a34
Steve Warren <steve-warren@users.noreply.github.com>
2022-05-25 17:55:50 -0500

    fix indentation issue.

--- bench/KsuidBenchmark.cs
+++ bench/KsuidBenchmark.cs
@@ -1,4 +1,3 @@
-using System.Security.Cryptography;
 using BenchmarkDotNet.Attributes;
 
 namespace KsuidDotNet.Bench;
@@ -9,36 +8,36 @@ public class KsuidBenchmark
     [Benchmark]
     public void KsuidDotNet_NewKsuid()
     {
-		_ = KsuidDotNet.Ksuid.NewKsuid();
-	}
+        _ = KsuidDotNet.Ksuid.NewKsuid();
+    }
 
     [Benchmark]
     public void StructKsuid_RandomKsuidToString()
     {
-		_ = StructKsuid.Ksuid.RandomKsuid().ToString();
-	}
+        _ = StructKsuid.Ksuid.RandomKsuid().ToString();
+    }
 
     [Benchmark]
     public void DotKsuid_GenerateToString()
     {
-		_ = KSUID.Ksuid.Generate().ToString();
-	}
+        _ = KSUID.Ksuid.Generate().ToString();
+    }
 
     [Benchmark]
     public void KsuidDotNet_NewKsuid_WithPrefix()
     {
-		_ = KsuidDotNet.Ksuid.NewKsuid("c_");
-	}
+        _ = KsuidDotNet.Ksuid.NewKsuid("c_");
+    }
 
     [Benchmark]
     public void StructKsuid_RandomKsuidToStringWithPrefix()
     {
-		_ = "c_" + StructKsuid.Ksuid.RandomKsuid().ToString();
-	}
+        _ = "c_" + StructKsuid.Ksuid.RandomKsuid().ToString();
+    }
 
     [Benchmark]
     public void DotKsuid_GenerateToStringWithPrefix()
     {
-		_ = "c_" + KSUID.Ksuid.Generate().ToString();
-	}
+        _ = "c_" + KSUID.Ksuid.Generate().ToString();
+    }
 }
